c351d64e835a7a0b1f752be5918c9147401b0149
RvStFyth <r.vansintfeyth@protonmail.com>
2022-11-19 16:02:38 +0100

    Support for same stream to multiple servers

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior} = require('@discordjs/voice');
 const config = require('./config.json');
 
@@ -9,10 +9,24 @@ const bot = new Client({
     ]
 });
 
+const resource = createAudioResource(config.stream_url, {
+    inlineVolume: true
+});
+const audioPlayer = createAudioPlayer({
+    behaviors: {
+        noSubscriber: NoSubscriberBehavior.Pause,
+    },
+});
+
+audioPlayer.play(resource);
+
 const validateConfig = () => {
     const keys = ['discord_token', 'channel_id', 'stream_url'];
     for (let x of keys) {
         if (!config[x]) {
+            if (process.env[x.toUpperCase()]) {
+                config[x] = process.env[x.toUpperCase()];
+            }
             console.error(`Missing property ${x} in config.json..`);
             return false;
         }
@@ -20,26 +34,24 @@ const validateConfig = () => {
     return true;
 }
 
-bot.on('ready', async () => {
+// For future usage, when slash commands gets implemented
+const connections = {};
+
+bot.on(Events.ClientReady, async () => {
     if (validateConfig()) {
-        const resource = createAudioResource(config.stream_url, {
-            inlineVolume: true
-        });
-        const audioPlayer = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-        const channel = await bot.channels.cache.get(config.channel_id);
-        if (channel) {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator
-            });
-            audioPlayer.play(resource);
-            connection.subscribe(audioPlayer);
-        } else console.error("Unable to fetch voice channel..");
+        const channels = config.channel_id.split(',');
+        for(let chan of channels) {
+            const channel = await bot.channels.cache.get(chan);
+            if (channel) {
+                const connection = joinVoiceChannel({
+                    channelId: channel.id,
+                    guildId: channel.guild.id,
+                    adapterCreator: channel.guild.voiceAdapterCreator
+                });
+                connection.subscribe(audioPlayer);
+                connections[channel.id] = connection;
+            } else console.error("Unable to fetch voice channel..");
+        }
     }
 });
 
