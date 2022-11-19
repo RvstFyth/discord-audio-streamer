10e4e0d7faf3da7d0d90ea67ae6b2468af695833
RvStFyth <r.vansintfeyth@protonmail.com>
2022-11-19 16:51:50 +0100

    refactor and reconnect to stream on error or idle state

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, Events } = require('discord.js');
-const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior, VoiceConnectionStatus, AudioPlayerStatus} = require('@discordjs/voice');
+
 const config = require('./config.json');
+const { validateConfig, getAudioPlayer, getVoiceConnection } = require('./utils.js');
 
 const bot = new Client({
     intents: [
@@ -8,66 +9,24 @@ const bot = new Client({
         GatewayIntentBits.GuildVoiceStates
     ]
 });
-
-const resource = createAudioResource(config.stream_url, {
-    inlineVolume: true
-});
-const audioPlayer = createAudioPlayer({
-    behaviors: {
-        noSubscriber: NoSubscriberBehavior.Pause,
-    },
-});
-
-audioPlayer.play(resource);
-audioPlayer.on(AudioPlayerStatus.Playing, () => {
-    console.log(`AudioPlayer: started playing`);
-});
-audioPlayer.on(AudioPlayerStatus.Buffering, () => {
-    console.log(`AudioPlayer: entered state Buffering`)
-});
-audioPlayer.on(AudioPlayerStatus.Idle, () => {
-    console.log(`AudioPlayer: entered state idle`)
-});
-audioPlayer.on('error', error => {
-    console.error(`AudioPlayer: Error.. ${error.message}`);
-});
-
-const validateConfig = () => {
-    const keys = ['discord_token', 'channel_id', 'stream_url'];
-    for (let x of keys) {
-        if (!config[x]) {
-            if (process.env[x.toUpperCase()]) {
-                config[x] = process.env[x.toUpperCase()];
-            }
-            console.error(`Missing property ${x} in config.json..`);
-            return false;
-        }
-    }
-    return true;
-}
+const audioPlayer = getAudioPlayer(config.stream_url);
 
 // For future usage, when slash commands gets implemented
 const connections = {};
+const connectedGuilds = [];
 
 bot.on(Events.ClientReady, async () => {
-    if (validateConfig()) {
+    if (validateConfig(config)) {
         const channels = config.channel_id.split(',');
         for(let chan of channels) {
             const channel = await bot.channels.cache.get(chan);
             if (channel) {
-                const connection = joinVoiceChannel({
-                    channelId: channel.id,
-                    guildId: channel.guild.id,
-                    adapterCreator: channel.guild.voiceAdapterCreator
-                });
-                connection.on(VoiceConnectionStatus.Ready, () => {
-                    console.log(`Connected to voice channel ${channel.name} in ${channel.guild.name} guild`);
-                });
-                connection.on(VoiceConnectionStatus.Disconnected, () => {
-                   console.log(`Disconnected from voice channel ${channel.name} in ${channel.guild.name} guild`);
-                });
-                connection.subscribe(audioPlayer);
-                connections[channel.id] = connection;
+                if (connectedGuilds.indexOf(channel.guildId) < 0) {
+                    connections[channel.id] = getVoiceConnection(channel);
+                    connections[channel.id].subscribe(audioPlayer);
+                    connectedGuilds.push(channel.guildId);
+                }
+                else console.error(`There is already a active connection for ${channel.guild.name}, ignoring ${channel.name}`);
             } else console.error("Unable to fetch voice channel..");
         }
     }
---
+++ src/utils.js
@@ -0,0 +1,76 @@
+const { createAudioPlayer, createAudioResource, AudioPlayerStatus, NoSubscriberBehavior, joinVoiceChannel, VoiceConnectionStatus } = require('@discordjs/voice');
+
+let audioPlayer;
+
+const validateConfig = (config) => {
+    const keys = ['discord_token', 'channel_id', 'stream_url'];
+    for (let x of keys) {
+        if (!config[x]) {
+            if (process.env[x.toUpperCase()]) {
+                config[x] = process.env[x.toUpperCase()];
+            }
+            console.error(`Missing property ${x} in config.json..`);
+            return false;
+        }
+    }
+    return true;
+}
+
+const getAudioPlayer = (streamUrl) => {
+    if (!audioPlayer) {
+        audioPlayer = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Pause,
+            },
+        });
+        const play = () => {
+            const resource = createAudioResource(streamUrl, {
+                inlineVolume: true
+            });
+            audioPlayer.play(resource);
+        }
+
+        audioPlayer.on(AudioPlayerStatus.Playing, () => {
+            console.log(`AudioPlayer: started playing`);
+        });
+        audioPlayer.on(AudioPlayerStatus.Buffering, () => {
+            console.log(`AudioPlayer: entered state Buffering`)
+        });
+        audioPlayer.on(AudioPlayerStatus.Idle, () => {
+            console.log(`AudioPlayer: entered state idle`);
+            console.log(`AudioPlayer: trying to reconnect in 10 seconds`);
+            setTimeout(play, 10 * 1000);
+        });
+        audioPlayer.on('error', error => {
+            console.error(`AudioPlayer: Error.. ${error.message}`);
+            console.log(`AudioPlayer: trying to reconnect in 10 seconds`);
+            setTimeout(play, 10 * 1000);
+        });
+
+        play();
+    }
+
+    return audioPlayer;
+}
+
+const getVoiceConnection = (channel) => {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator
+    });
+    connection.on(VoiceConnectionStatus.Ready, () => {
+        console.log(`Connected to voice channel ${channel.name} in ${channel.guild.name} guild`);
+    });
+    connection.on(VoiceConnectionStatus.Disconnected, () => {
+        console.log(`Disconnected from voice channel ${channel.name} in ${channel.guild.name} guild`);
+    });
+
+    return connection;
+}
+
+module.exports = {
+    validateConfig,
+    getAudioPlayer,
+    getVoiceConnection
+}
